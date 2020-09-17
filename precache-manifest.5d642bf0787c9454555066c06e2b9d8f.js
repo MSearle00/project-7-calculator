44a28028d9002f6be35b29bf5fb23671524fd5df
Maria Searle <maria.searle@deeperthanblue.co.uk>
2020-09-17 11:25:17 +0100

    Updates

---
+++ precache-manifest.5d642bf0787c9454555066c06e2b9d8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0397b85024bd3f132ba6c51662b89dca",
+    "url": "/project-7-calculator/index.html"
+  },
+  {
+    "revision": "a4960d954f20e1d4ce91",
+    "url": "/project-7-calculator/static/css/main.814d2d17.chunk.css"
+  },
+  {
+    "revision": "48926d9c80cce50b0776",
+    "url": "/project-7-calculator/static/js/2.2135fec4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-7-calculator/static/js/2.2135fec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4960d954f20e1d4ce91",
+    "url": "/project-7-calculator/static/js/main.01ab5d35.chunk.js"
+  },
+  {
+    "revision": "fb9adebbb4663dbacf7d",
+    "url": "/project-7-calculator/static/js/runtime-main.144cbd05.js"
+  }
+]);
